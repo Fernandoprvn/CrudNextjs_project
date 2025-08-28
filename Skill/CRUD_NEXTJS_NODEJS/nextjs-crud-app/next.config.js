10429808362cabf65ae8e664ba597f758330067c
Praveen fernando w <106607950+Fernandoprvn@users.noreply.github.com>
2025-08-28 21:54:10 +0530

    Create next.config.js

---
+++ Skill/CRUD_NEXTJS_NODEJS/nextjs-crud-app/next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true, // optional, helps with static routing
+};
+
+module.exports = nextConfig;
